020b1733e9136656c69a12a9ebe775a692d6c26a
mathibenitez <matbenitez390@gmail.com>
2023-04-28 20:00:09 -0300

    feat: :fire: Parse req.body

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ import express from 'express';
 const PORT = 3000;
 const expressApp = express();
 
+expressApp.use(express.json());
+expressApp.use(express.text());
 
-expressApp.get("/cuenta/:idCuenta", (req, res) => {
-    console.log(req.params.idCuenta)
+expressApp.post("/cuenta/:idCuenta", (req, res) => {
+console.log(req.body);
+
     res.send("Tu cuenta personal");
+})
+
+expressApp.put("/producto", (req, res) => {
+console.log(req.body);
+
+    res.send("Un producto");
 })
 
 expressApp.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}`));
