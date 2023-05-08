3362792ef483ad10d34b975dec2e8ae547fff1cf
mathibenitez <84646522+mathibenitez@users.noreply.github.com>
2023-05-08 11:53:38 -0300

    feat: :rocket: Mongodb test

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
-import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
 import accountRouter from './routers/account.js';
 import authRouter from './routers/auth.js';
-import authTokenRouter from './routers/auth_token.js';
 import authSessionRouter from './routers/auth_session.js';
+import authTokenRouter from './routers/auth_token.js';
 
 dotenv.config();
 
@@ -22,4 +23,10 @@ expressApp.use('/auth', authRouter);
 expressApp.use('/auth-token', authTokenRouter);
 expressApp.use('/auth-session', authSessionRouter);
 
-expressApp.listen(PORT, () => console.log(`Server up in port: ${PORT}`));
+const bootstrap = async () => {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    expressApp.listen(PORT, () => console.log(`Server up in port: ${PORT}`));
+}
+
+bootstrap();
